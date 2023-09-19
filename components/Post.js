f9ea5a19fb6e06d310363ce518b4e64e6432cf71
Raja Naveed <rajadahri11@gmail.com>
2023-09-20 04:27:49 +0500

    added like functionality

--- components/Post.js
+++ components/Post.js
@@ -1,16 +1,52 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { MdOutlineClose } from "react-icons/md";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { FaGlobeAmericas } from "react-icons/fa";
-import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai"; // Add AiFillLike for the filled like icon
 import { TfiComment } from "react-icons/tfi";
 import { useSession } from "next-auth/react";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
 import { db } from "../firebase";
 
-
 const Post = ({ data, id }) => {
-  const { data: session } = useSession();
+    const { data: session } = useSession();
+    console.log(session, "session")
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(data.likes ? data.likes.length : 0);
+
+  useEffect(() => {
+    // Check if the current user has already liked the post
+    if (data.likes && session) {
+      setLiked(data.likes.includes(session.user.uid));
+    }
+  }, [data.likes, session]);
+
+  const toggleLike = async () => {
+    // Check if the user is authenticated
+    if (!session) {
+      // Handle the case where the user is not authenticated
+      return;
+    }
+
+    const postRef = doc(db, "posts", id);
+
+    if (!liked) {
+      // If the user hasn't liked the post, add their UID to the likes array
+      await updateDoc(postRef, {
+        likes: arrayUnion(session.user.uid),
+      });
+      setLikesCount(likesCount + 1);
+    } else {
+      // If the user has liked the post, remove their UID from the likes array
+      await updateDoc(postRef, {
+        likes: arrayRemove(session.user.uid),
+      });
+      setLikesCount(likesCount - 1);
+    }
+
+    // Toggle the liked state
+    setLiked(!liked);
+  };
 
   const isAdmin = (post_data_id, session_id) => {
     if (post_data_id === session_id) return true;
@@ -61,10 +97,10 @@ const Post = ({ data, id }) => {
       <div className="mx-4 h-[1px] bg-gray-300 mt-[15px]"></div>
 
       <div className="flex mt-[7px] text-gray-500">
-        <div className="flex gap-2 justify-center items-center w-[50%] py-2 rounded-[10px] hover:bg-gray-200 cursor-pointer">
-          <AiOutlineLike className="text-[26px]" />
-          <p className="font-medium"  > Like</p>
-        </div>
+      <div className="flex gap-2 justify-center items-center w-[50%] py-2 rounded-[10px] hover:bg-gray-200 cursor-pointer" onClick={toggleLike}>
+        {liked ? <AiFillLike className="text-[26px]" /> : <AiOutlineLike className="text-[26px]" />}
+        <p className="font-medium">{likesCount} Likes</p>
+      </div>
         <div className="flex gap-2 justify-center items-center w-[50%] py-2 rounded-[10px] hover:bg-gray-200 cursor-pointer">
           <TfiComment className="text-[20px] translate-y-[4px]" />
           <p className="font-medium">Comment</p>
