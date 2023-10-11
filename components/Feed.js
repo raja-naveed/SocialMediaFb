143715d14d47f5c332e2d17f2f22bed495d94e40
Raja Naveed <rajadahri11@gmail.com>
2023-10-11 23:58:25 +0500

    added new library infinite scroll

--- components/Feed.js
+++ components/Feed.js
@@ -4,9 +4,16 @@ import WhatsOnYourMind from "./WhatsOnYourMind";
 import Post from "./Post";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
+  const [hasMore, setHasMore] = useState(true); // Set this to false when there are no more posts to load
+
+  const fetchMorePosts = () => {
+    // Fetch more posts here and add them to the 'posts' state
+    // You can use a similar query with offset and limit
+  };
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -17,7 +24,6 @@ const Feed = () => {
     );
 
     return () => {
-      // Unsubscribe from the snapshot listener when the component unmounts
       unsubscribe();
     };
   }, []);
@@ -26,9 +32,16 @@ const Feed = () => {
     <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
       <Story />
       <WhatsOnYourMind />
-      {posts.map((post) => {
-        return <Post key={post.id} id={post.id} data={post.data()} />;
-      })}
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={fetchMorePosts}
+        hasMore={hasMore}
+        loader={<h4>Loading more posts...</h4>}
+      >
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} data={post.data()} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 };
